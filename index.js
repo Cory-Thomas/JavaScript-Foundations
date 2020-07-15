7422a84a67789e866e15669909f31a1b0691b5d3
Cory Thomas <cory.a.thomas1992@gmail.com>
2020-07-14 21:53:12 -0400

    A little past halfway through the directions

--- index.js
+++ index.js
@@ -4,8 +4,10 @@
 /* Create variables for principal, interest rate, and years. Assign them the values 200000, 0.05, and 30 respectively. Create another value called name and give it the value of your own name.
 */
 
-
-
+// let principal = 200000;
+// let interestRate = 0.05;
+// let years = 30;
+const name = "Cory";
 
 
 // 🏡 Task 1.5: Simple Math
@@ -15,7 +17,8 @@
 (2) Create another variable called `periods` and give it the value of years*12.
 */
 
-
+// let monthlyInterestRate = interestRate / 12;
+// let periods = years * 12;
 
 
 // 🏡 Task 2: Harder Math
@@ -35,16 +38,19 @@ Hint #2: you'll need to use the `math` object for parts of this calculation!
 When your math is correct, monthlyRate will equal 1073.64
 */
 
-
+let monthlyRate;
 
 
 // 🏡 Task 3: Function
-/* Create a function called `mortgageCalculator` that combines all of the steps from task 1 and 2 and returns a sentence "{Name}, your monthly rate is ${monthlyRate}"
+/* Create a function called `mortgageCalculator` that combines all of the steps from task 1 and 2 and returns a sentence "{Name}, 
+your monthly rate is ${monthlyRate}"
 
 If your name is `Oscar` mortgageCalculator() should return "Oscar, your monthly rate is 1073.64"
 */
 
 
+
+// console.log(mortgageCalculator())
 
 
 
@@ -55,19 +61,41 @@ For example,
 mortgageCalculator(200000, 0.05, 30); <-- should return 1,073.64
 */
 
+// const mortgageCalculator = (principal, interestRate, years) => {
+    
+    
+//     let monthlyInterestRate = interestRate / 12;
+//     let periods = years * 12;
 
+//     monthlyRate = ( principal * ( monthlyInterestRate * Math.pow( ( 1 + monthlyInterestRate ), periods ) ) / ( Math.pow( ( 1 + monthlyInterestRate ), periods ) - 1 ) );
+    
+//     return `${name}, your monthly rate is ${monthlyRate}`;
+// };
 
-
+// mortgageCalculator(200000, 0.05, 30);
 
 // 🏡 Task 5: Conditionals
 /* Add another paramter to your function called credit score. This parameter will be a number between 0 and 800 (a credit score).
 
-Then, add control flow within your function such that IF creditScore is above 740, interest rate drops by 0.5%, if credit score is below 660, interest rate increases by 0.5% and if credit score is anywhere between 660 and 740 interest rate doesn't change.
+Then, add control flow within your function such that IF creditScore is above 740, interest rate drops by 0.5%, if credit score is below 660, 
+interest rate increases by 0.5% and if credit score is anywhere between 660 and 740 interest rate doesn't change.
 
-Hint: To drop an interest rate by 5% you can take monthlyRate and multiply it by 0.95. Similarly, to increase an interest rate by 5% you'd do monthlyRate * 1.05. 
+Hint: To drop an interest rate by 5% you can take monthlyRate and multiply it by 0.95. 
+Similarly, to increase an interest rate by 5% you'd do monthlyRate * 1.05. 
 */
 
+const mortgageCalculator = (principal, interestRate, years, credit) => {
+    
+    
+    let monthlyInterestRate = interestRate / 12;
+    let periods = years * 12;
 
+    monthlyRate = ( principal * ( monthlyInterestRate * Math.pow( ( 1 + monthlyInterestRate ), periods ) ) / ( Math.pow( ( 1 + monthlyInterestRate ), periods ) - 1 ) );
+    
+    return `${name}, your monthly rate is ${monthlyRate}`;
+};
+
+mortgageCalculator(200000, 0.05, 30);
 
 
 // 🏡 Task 6: Loops
